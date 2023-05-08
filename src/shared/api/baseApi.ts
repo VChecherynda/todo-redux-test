2aee10db485557015f604261890320250c56d890
vadym <checherinda.vadym@webbylab.com.com>
2023-05-09 02:25:22 +0300

    Fix baseApi type

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,5 +1,5 @@
 import { mockServerDB } from "./mocks/mockServerDB";
-import { Issue } from "../../shared/types";
+import { Issue } from "../../entities/issue";
 
 export const queryIssues = async (): Promise<Issue[]> => {
     return await new Promise((resolve) => setTimeout(() => resolve(mockServerDB.issues as Issue[]), 1000));
